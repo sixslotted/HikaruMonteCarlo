31a59bc6bc924134d74a0e94ddce071172c72c65
Eddie Calmo <kenbarron@gmail.com>
2023-12-12 00:04:27 +0000

    added draw estimation function

--- hikaruCheck.py
+++ hikaruCheck.py
@@ -8,27 +8,28 @@ Created on Wed Nov 29 12:00:30 2023
 import requests
 import chess
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-headers = {'User-Agent': 'username: Takin_These, email: kenbarron@gmail.com'}
-URL = 'https://api.chess.com/pub/player/hikaru/games/archives'
-
-data = requests.get(URL, headers=headers)
-print('data='+ str(data.status_code))
-datajson=data.json()
-
-hikaruGames = []
-for index,url in enumerate(reversed(datajson['archives'])):
-    #print (url)
-    currentGames = requests.get(url,headers=headers).json()
-    for game in currentGames['games']:
-        thisGameData = {
-            'white' : game['white'],
-            'black' : game['black']
-            }
-        if game['time_class']=='blitz':
-            hikaruGames.append(thisGameData)
-    if index == 11:
-        break
+def CalculateDrawChance (gameSet):
+    winDrawDistribution = []
+    maxDifference = 0
+    for game in gameSet:
+        ratingDifference = abs(game['white']['rating']-game['black']['rating'])
+        while (ratingDifference - maxDifference >= 0):
+            maxDifference +=10
+            winDrawDistribution.append([maxDifference,0,0,0])
+        if game['white']['result']=='win' or game['black']['result']=='win':
+            winDrawDistribution[ratingDifference // 10 ][1]+=1
+        else:
+            winDrawDistribution[ratingDifference // 10 ][2]+=1
+    for interval in winDrawDistribution:
+        if interval[2] + interval [1] > 0:
+            interval[3]=interval[1]/(interval[1]+interval[2])
+    arrayFormat = np.array(winDrawDistribution)
+    plt.plot(arrayFormat[0,:])
+    plt.show()
+    return winDrawDistribution
 
 def Simulate (gameSet):
     winRecord = []
@@ -49,3 +50,28 @@ def Simulate (gameSet):
 
 def GlickoCalculateWinChance(rating1,rating2):
     return 1/(1+10**-((rating1-rating2)/400))
+
+headers = {'User-Agent': 'username: Takin_These, email: kenbarron@gmail.com'}
+URL = 'https://api.chess.com/pub/player/hikaru/games/archives'
+
+data = requests.get(URL, headers=headers)
+print('data='+ str(data.status_code))
+datajson=data.json()
+
+hikaruGames = []
+for index,url in enumerate(reversed(datajson['archives'])):
+    #print (url)
+    currentGames = requests.get(url,headers=headers).json()
+    for game in currentGames['games']:
+        if game['time_class']=='blitz':
+            thisGameData = {
+            'white' : game['white'],
+            'black' : game['black']
+            }
+            hikaruGames.append(thisGameData)
+    if index == 11:
+        break
+
+winDrawChances = CalculateDrawChance (hikaruGames)
+results = Simulate(hikaruGames)
+
